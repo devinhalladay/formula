3e4ae35f365781551358bc0104ef64d79cf1855d
Devin Halladay <devin@devinhalladay.com>
2016-07-11 22:00:44 -0400

    Changing env var names and extracting some data to env variables

--- sendgrid_mailer.rb
+++ sendgrid_mailer.rb
@@ -1,30 +1,32 @@
 require 'sinatra'
 require 'pony'
 
-origin_domain = ENV['origin_domain']
+# ENV['ORIGIN_DOMAIN'] should be formatted as http://domain.extension/
+origin_domain = ENV['ORIGIN_DOMAIN']
 set :protection, :origin_whitelist => origin_domain
 
 get '/' do
-  redirect 'http://devinhalladay.com/contact'
+  redirect ENV['GET_REDIRECT_URL']
 end
 
 post '/' do
   Pony.mail({
-    :to => ENV['recipient'],
     :from => params[:email],
+    :to => ENV['RECIPIENT_EMAIL_ADDRESS'],
     :subject => "New inquiry from #{params[:name]}",
     :body => erb(:email),
     :via => :smtp,
     :via_options => {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
-      :domain         => "heroku.com", # the HELO domain provided by the client to the server
-      :enable_starttls_auto => true
+      :address              => 'smtp.sendgrid.net',
+      :port                 => '587',
+      :enable_starttls_auto => true,
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
+      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+      :domain               => "heroku.com" # the HELO domain provided by the client to the server
     }
   })
 
-  redirect 'http://devinhalladay.com/contact#success'
+  redirect ENV['REDIRECT_URL']
 end
+http://devinhalladay.com/contact#success
