3cc8460a104ef6accfd2a6c067702e815d95a54b
Devin Halladay <devin@devinhalladay.com>
2016-07-12 16:32:52 -0400

    Adding server-side form validation

--- mailer.rb
+++ mailer.rb
@@ -1,29 +1,73 @@
 require 'sinatra'
 require 'pony'
 
-origin_domain = ENV['ORIGIN_DOMAIN'] # ENV['ORIGIN_DOMAIN'] should be formatted as http://domain.extension/
-set :protection, :origin_whitelist => origin_domain # Only allow POST from specified origins. You may specify multiple whitelisted domains by separating domains in ENV['ORIGIN_DOMAIN'] by commas.
+origin_domain = ENV['ORIGIN_DOMAIN']
+set :protection, :origin_whitelist => origin_domain # Only allow POST from specified origins.
+
+Pony.options = {
+  :via => :smtp,
+  :via_options => {
+    :address              => 'smtp.sendgrid.net',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "heroku.com" # the HELO domain provided by the client to the server
+  }
+}
+
+helpers do
+  def valid_name?(name)
+    true if name && !name.empty?
+  end
+
+  def valid_email?(email)
+    email_match = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/
+    true if email && !(email =~ email_match).nil?
+  end
+
+  def valid_message?(message)
+    true if message && !message.empty?
+  end
+end
 
 get '/' do
   redirect ENV['GET_REDIRECT_URL']
 end
 
 post '/' do
-  Pony.mail({
-    :from => params[:email],
-    :to => ENV['RECIPIENT_EMAIL_ADDRESS'],
-    :subject => "New email from your static form!",
-    :body => erb(:email),
-    :via => :smtp,
-    :via_options => {
-      :address              => 'smtp.sendgrid.net',
-      :port                 => '587',
-      :enable_starttls_auto => true,
-      :user_name            => ENV['SENDGRID_USERNAME'],
-      :password             => ENV['SENDGRID_PASSWORD'],
-      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-      :domain               => "heroku.com" # the HELO domain provided by the client to the server
-    }
-  })
-  redirect ENV['POST_REDIRECT_URL'] # ENV['POST_REDIRECT_URL'] should be formatted as http://domain.extension/
+  return jsonp
+
+  @errors = {}
+  @failure = false
+
+  if !valid_name?(params[:name])
+    @errors[:name] = true
+    @failure = true
+  end
+
+  if !valid_email?(params[:email])
+    @errors[:email] = true
+    @failure = true
+  end
+
+  if !valid_message?(params[:message])
+    @errors[:message] = true
+    @failure = true
+  end
+
+  if @failure
+    return jsonp @errors
+  else
+    Pony.mail({
+      :from => params[:email],
+      :to => ENV['RECIPIENT_EMAIL_ADDRESS'],
+      :subject => "New email from your static form!",
+      :body => erb(:email)
+    })
+    redirect ENV['POST_REDIRECT_URL']
+  end
+
+  return jsonp true
 end
